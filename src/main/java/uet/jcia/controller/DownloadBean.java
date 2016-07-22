6c66f1cb6e2332c438fc1bcb8c73964a012693ae
dinhtienloc <dinhtienloc1996@gmail.com>
2016-07-22 14:02:39 +0700

    add DownloadBean

--- src/main/java/uet/jcia/controller/DownloadBean.java
+++ src/main/java/uet/jcia/controller/DownloadBean.java
@@ -1,5 +1,10 @@
 package uet.jcia.controller;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 import javax.annotation.ManagedBean;
@@ -15,22 +20,47 @@ import uet.jcia.model.CoreAPI;
 @SessionScoped
 public class DownloadBean {
 	@SuppressWarnings("unchecked")
-	public void download() {
+	public String getDownloadDir() {
 		FacesContext facesContext = FacesContext.getCurrentInstance();
 		ExternalContext exContext = facesContext.getExternalContext();
-	    HttpSession session = (HttpSession) exContext.getSession(false);
-	    
-	    String sessionid = session.getId();
-	    String ssTableKey = sessionid + "table";
-	    
-	    String resultDir = (String) session.getAttribute(sessionid);
+		HttpSession session = (HttpSession) exContext.getSession(false);
+
+		String sessionid = session.getId();
+		String ssTableKey = sessionid + "table";
+
+		String resultDir = (String) session.getAttribute(sessionid);
 		List<Table> list = (List<Table>) session.getAttribute(ssTableKey);
-		
+
 		CoreAPI api = new CoreAPI();
-		for(Table t : list) {
+		for (Table t : list) {
 			api.updateTable(t);
 		}
+
+		String downloadDir = api.download(resultDir);
+		return downloadDir;
+	}
+
+	public void download() {
+		String downloadDir = getDownloadDir();
+		File file = new File(downloadDir);
+		try {
+			InputStream fis = new FileInputStream(file);
+			byte[] buf = new byte[1024];
+			int offset = 0;
+			int numRead = 0;
+			while ((offset < buf.length) && ((numRead = fis.read(buf, offset, buf.length - offset)) >= 0)) {
+				offset += numRead;
+			}
+			fis.close();
+			
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-		String downloadURL = api.download(resultDir);
 	}
 }
