9b66de5094d48036addd033b07ead81d91db7db3
bqcuong2212 <bqcuong2212@gmail.com>
2016-07-25 21:50:38 +0700

    save document on external memory

--- src/main/java/uet/jcia/model/FileManager.java
+++ src/main/java/uet/jcia/model/FileManager.java
@@ -11,7 +11,10 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+
+import org.w3c.dom.Document;
 
 import uet.jcia.entities.Table;
 import uet.jcia.utils.Constants;
@@ -38,6 +41,26 @@ public class FileManager {
             return null;
         }
     }
+
+    public String saveDocumentsHash(HashMap<String, Document> documents) {
+        Date d = new Date();
+        String filePath = Constants.TEMP_SOURCE_FOLDER + File.separator
+                + "document-data-" + Helper.DATE_FORMATER.format(d);
+        
+        try {
+            FileOutputStream fos = new FileOutputStream(filePath);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(documents);
+            
+            return filePath;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
     
     public List<Table> readTables(String filePath) {
         try {
@@ -47,6 +70,23 @@ public class FileManager {
             List<Table> list = (List<Table>) ois.readObject();
             
             return list;
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+            
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        
+        return null;
+    }
+
+    public HashMap<String, Document> readDocumentsHash(String filePath) {
+        try {
+            FileInputStream fis = new FileInputStream(filePath);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            HashMap<String, Document> hash = (HashMap<String, Document>) ois.readObject();
+            return hash;
             
         } catch (IOException e) {
             e.printStackTrace();
