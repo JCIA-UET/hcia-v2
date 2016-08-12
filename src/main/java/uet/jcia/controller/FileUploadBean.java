457c0c9a407e7df926eb685a8c23be08ada44504
hieusonson9x <hieusonson9x@gmail.com>
2016-08-12 21:37:05 +0700

    fix bug

---
+++ src/main/java/uet/jcia/controller/FileUploadBean.java
@@ -0,0 +1,140 @@
+package uet.jcia.controller;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.Part;
+
+import uet.jcia.entities.TreeNode;
+import uet.jcia.model.CoreAPI;
+import uet.jcia.utils.Constants;
+
+@ManagedBean(name="fileUploadBean")
+@SessionScoped
+public class FileUploadBean implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Part file;
+
+	private boolean validPass;
+
+	public boolean isValidPass() {
+		return validPass;
+	}
+
+	public void setValidPass(boolean validPass) {
+		this.validPass = validPass;
+	}
+
+	public Part getFile() {
+		return file;
+	}
+
+	public void setFile(Part file) {
+		this.file = file;
+	}
+
+	public void upload() {
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		ExternalContext exContext = facesContext.getExternalContext();
+	    HttpSession session = (HttpSession) exContext.getSession(true);
+
+		CoreAPI core = new CoreAPI();
+
+		String fileName = getFileName(file);
+		InputStream is;
+		try {
+			is = file.getInputStream();
+			
+			String fileDir = saveFile(is, fileName);
+			System.out.println("Saved file's directory: " + fileDir);
+			String parsedResultDir = null;
+			try {
+				parsedResultDir = core.parse(fileDir);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println("Parsed original file's directory: " + parsedResultDir);
+			
+			if(parsedResultDir != null) {
+				String sessionid = session.getId();
+
+				String logKey = sessionid + "log";
+				List<String> changeLog = new ArrayList<>();
+				File tempFile = new File(parsedResultDir);
+				changeLog.add(tempFile.getName());
+				exContext.getSessionMap().put(logKey, changeLog);
+				
+				String originDirKey = sessionid + "origindir";
+				exContext.getSessionMap().put(originDirKey, parsedResultDir);
+				
+				
+			}
+			
+			exContext.redirect("index.xhtml");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void validateFile(FacesContext ctx, UIComponent comp, Object value) {
+		FacesMessage msg = null;
+		Part file = (Part) value;
+		this.validPass = false;
+
+		String fileName = file.getSubmittedFileName();
+		if (file.getSize() == 0) {
+			msg = new FacesMessage("File is required to upload.");
+			msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+		} else if (!fileName.endsWith("zip") && !fileName.endsWith("hbm.xml")) {
+			msg = new FacesMessage("Allowed extensions: zip, hbm.xml");
+			msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+		}
+
+		if (msg == null) {
+			this.validPass = true;
+		} else if (!(msg == null)) {
+			FacesContext.getCurrentInstance().addMessage("input-file", msg);
+			throw new ValidatorException(msg);
+		}
+	}
+
+	private static String getFileName(Part part) {
+		return part.getSubmittedFileName();
+	}
+
+	private String saveFile(InputStream is, String fileName) {
+		byte[] buffer = new byte[1024000];
+		String fileDir = null;
+		try {
+			fileDir = Constants.TEMP_SOURCE_FOLDER + File.separator + fileName;
+			BufferedOutputStream bof = new BufferedOutputStream(new FileOutputStream(new File(fileDir)));
+			int i = -1;
+			while ((i = is.read(buffer)) != -1) {
+				bof.write(buffer, 0, i);
+			}
+			bof.close();
+			return fileDir;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
